b79b6d1d5aee607bc18d69d0ccc941982d301c97
Juan David <jdgranadillo9@gmail.com>
2022-05-22 21:25:23 -0400

    little changes

---
+++ x.js
@@ -0,0 +1,6 @@
+let pi = 3.14;
+let radio = prompt("introduzca radio: ");
+let longitud = 2*pi*radio;
+let area = pi*(radio**2);
+let volumen = (4/3)*pi*(radio**3);
+document.write(`Longitud: ${longitud}<br> Área: ${area}<br> Volúmen: ${volumen}`);
